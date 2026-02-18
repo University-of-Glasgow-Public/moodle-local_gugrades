99459444e59e393e33e101d282a2bae64d45a3df
mdclark92 <150026446+mdclark92@users.noreply.github.com>
2026-02-18 09:37:53 +0000

    Removing white space

--- tests/behat/behat_local_gugrades.php
+++ tests/behat/behat_local_gugrades.php
@@ -31,7 +31,7 @@ class behat_local_gugrades extends behat_base {
       * Creates URLS
       *
       * @param string $type
-      * 
+      *
       * @return string
       */
     protected function resolve_page_url(string $type): moodle_url {
