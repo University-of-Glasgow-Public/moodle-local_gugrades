2ced54d7c4105299da41b4f4a4da6f84ec5fe272
Howard Miller <howardsmiller@gmail.com>
2026-03-03 14:06:36 +0000

    starting to add tailwind stuff

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,11 +3,14 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
