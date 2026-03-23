da3b2a61f8362ea4d33f5f8b60798b707875f263
Howard Miller <howardsmiller@gmail.com>
2026-03-23 16:51:59 +0000

    adding more stuff to ts and whatnot

---
+++ ui/src/js/moodlefetch.ts
@@ -0,0 +1,24 @@
+declare global {
+  interface Window {
+      GU: GUType;
+  }
+}
+
+interface GUType {
+    courseid: BigInteger,
+    fetchMany: CallableFunction,
+}
+
+export const moodleFetch = (methodname: string, args: object) => {
+
+    const GU = window.GU;
+    const fetchMany = GU.fetchMany;
+    const courseid = GU.courseid;
+
+    args['courseid'] = courseid;
+
+    return fetchMany([{
+        methodname: methodname,
+        args: args
+    }])[0];
+}
