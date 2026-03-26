62a173c4151d2db8c4ab99def64cfc53122dbef7
Howard Miller <howardsmiller@gmail.com>
2026-03-26 09:46:38 +0000

    fixing info and refresh buttons on capture page

--- ui/src/components/ReloadButton.vue
+++ ui/src/components/ReloadButton.vue
@@ -1,15 +1,20 @@
 <template>
     <!-- Refresh button -->
-    <div href="#" id="refresh_button" class="tw:ml-3 tw:tooltip" data-toggle="tooltip" :data-tip="mstrings.refreshpage" @click="refresh_clicked">
-        <i class="fa fa-refresh fa-Xl align-middle" :class="[fasizeclass]" aria-hidden="true"></i>
+    <div class="tw:ml-2 tw:tooltip"  @click="refresh_clicked" :data-tip="mstrings['refreshpage']">
+        <button class="tw:btn">
+            <ArrowPathIcon class="tw:size-6 tw:text-black-500"></ArrowPathIcon>
+        </button>
     </div>
 </template>
 
-<script setup>
-    import {inject, defineEmits, onMounted} from '@vue/runtime-core';
+<script setup lang="ts">
+    import { storeToRefs } from 'pinia';
+    import { useMstrings } from '@/stores/mstrings.js';
+    import { ArrowPathIcon } from '@heroicons/vue/24/outline'
 
-    const mstrings = inject('mstrings');
     const emit = defineEmits(['refreshtable']);
+    const mstringstore = useMstrings();
+    const { mstrings } = storeToRefs( mstringstore );
 
     /**
      * Refresh button clicked
@@ -17,19 +22,4 @@
     function refresh_clicked() {
         emit('refreshtable');
     }
-
-    /**
-     * Initialise tooltip
-     */
-     onMounted(() => {
-        return;
-
-        // Bodge to get jQuery needed for Bootstrap JS.
-        const $ = window.jQuery;
-
-        $(function () {
-            $('#refresh_button').tooltip()
-        });
-    });
-
 </script>
