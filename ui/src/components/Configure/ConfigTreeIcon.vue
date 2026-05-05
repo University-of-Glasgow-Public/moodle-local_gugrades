60f8e6718a62bfa5a278f25a320b4ba55717112d
Howard Miller <howardsmiller@gmail.com>
2026-05-05 15:31:18 +0100

    fixing broken icons on configure page

--- ui/src/components/Configure/ConfigTreeIcon.vue
+++ ui/src/components/Configure/ConfigTreeIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <img v-if="gradeitem.itemtype != 'manual'" class="icon itemicon" :title="gradeitem.info.itemmodule" :src="gradeitem.icon" />
+    <img v-if="gradeitem.itemtype != 'manual'" class="w-6 h-6 inline-block object-contain" :title="gradeitem.info.itemmodule" :src="gradeitem.icon" />
     <i v-if="gradeitem.itemtype == 'manual'" class="icon fa fa-pen-to-square fa-fw icon itemicon" role="img" :title="gradeitem.info.itemmodule" aria-label="Manual item"></i>
 </template>
 
